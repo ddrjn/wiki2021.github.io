220370baadc40aeb6f7cc4b46fc0f83230c07739
Davit Rizhinashvili <rizhinashvilidavid@gmail.com>
2021-10-15 22:26:14 +0300

    reseter js

---
+++ res/js/reset.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    document.getElementById("HQ_page").id = "IDONTNEEDYOURCSS";
+    document.getElementById("bodyContent").id = "IDONTNEEDYOURCSS1";
+    document.getElementById("mw-content-text").id = "IDONTNEEDYOURCSS2";
+    document.getElementsByClassName("mw-content-ltr")[0].classList.remove("mw-content-ltr");
+}
